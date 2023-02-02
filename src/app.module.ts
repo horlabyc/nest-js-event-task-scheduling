a3be7bff24b5731b2faaa96864866eb35cd692e2
Olaosebikan Sulaiman <olabisisulaiman@gmail.com>
2023-02-02 09:16:55 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskService } from './task.service';
 
 @Module({
   imports: [EventEmitterModule.forRoot(), ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TaskService],
 })
 export class AppModule {}
